3c70475e5559d3ec0038af84bbc7902dfebd8a0d
Brian Picciano <bgp2396@bellsouth.net>
2013-09-28 12:43:30 -0400

    split up redis functionality into more files

---
+++ src/hyrax-server/storage/redis/command.go
@@ -0,0 +1,40 @@
+package redis
+
+import (
+	"github.com/mediocregopher/hyrax/src/hyrax-server/storage/command"
+)
+
+type RedisCommand struct {
+	cmd  []byte
+	args []interface{}
+	trans []command.Command
+}
+
+func NewRedisCommand(cmd []byte, args []interface{}) command.Command {
+	return &RedisCommand{
+		cmd: cmd,
+		args: args,
+		trans: nil,
+	}
+}
+
+func NewRedisTransaction(cmds ...command.Command) command.Command {
+	return &RedisCommand{
+		cmd: nil,
+		args: nil,
+		trans: cmds,
+	}
+}
+
+func (c *RedisCommand) Cmd() []byte {
+	return c.cmd
+}
+
+func (c *RedisCommand) Args() []interface{} {
+	return c.args
+}
+
+func (c *RedisCommand) ExpandTransaction() []command.Command {
+	return c.trans
+}
+
--- src/hyrax-server/storage/redis/factory.go
+++ src/hyrax-server/storage/redis/factory.go
@@ -24,41 +24,6 @@ var SCARD = []byte("SCARD")
 var MULTI = []byte("MULTI")
 var EXEC = []byte("EXEC")
 
-type RedisCommand struct {
-	cmd  []byte
-	args []interface{}
-	trans []command.Command
-}
-
-func NewRedisCommand(cmd []byte, args []interface{}) command.Command {
-	return &RedisCommand{
-		cmd: cmd,
-		args: args,
-		trans: nil,
-	}
-}
-
-func NewRedisTransaction(cmds ...command.Command) command.Command {
-	return &RedisCommand{
-		cmd: nil,
-		args: nil,
-		trans: cmds,
-	}
-}
-
-func (c *RedisCommand) Cmd() []byte {
-	return c.cmd
-}
-
-func (c *RedisCommand) Args() []interface{} {
-	return c.args
-}
-
-func (c *RedisCommand) ExpandTransaction() []command.Command {
-	return c.trans
-}
-
-////////////////////////////////////////////////////////////////////////////////
 
 type RedisCommandFactory struct{}
 
