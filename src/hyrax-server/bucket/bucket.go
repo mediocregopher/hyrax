a4c0482f2c83f93753657affa7096412c248d497
Brian Picciano <bgp2396@bellsouth.net>
2013-09-28 14:49:45 -0400

    added bucket operations

---
+++ src/hyrax-server/bucket/bucket.go
@@ -0,0 +1,63 @@
+package bucket
+
+import (
+	"sync"
+	"fmt"
+	"github.com/mediocregopher/hyrax/src/hyrax/types"
+)
+
+var bList []*string
+var bLock sync.RWMutex
+
+// Set sets the bucket at bIndex to the given string. The index must be inside
+// the existing bucket list, or be exactly one position outside of it. In
+// effect, this method is SetOrAppend
+func Set(s *string, bIndex int) error {
+	bLock.Lock()
+	defer bLock.Unlock()
+
+	bLen := len(bList)
+	if bIndex > bLen {
+		return fmt.Errorf("The bucket list length is %d, its size can only be increased by one, so it's maximum index right now is %d", bLen, bLen)
+	} else if bIndex == bLen {
+		bList = append(bList,s)
+	} else {
+		bList[bIndex] = s
+	}
+	return nil
+}
+
+// PrintBuckets is a debug command which simply dumps the bucket list to stdout
+func PrintBuckets() {
+	bLock.RLock()
+	defer bLock.RUnlock()
+	for i, s := range bList {
+		if s != nil {
+			fmt.Println(i,*s)
+		}
+	}
+}
+
+// KeyBucket hashes the given key with a Shift-Add-XOR hash, identifies which
+// bucket it belongs in based on this hash, and returns the string at that
+// bucket location.
+func KeyBucket(b types.Byter) (*string, error) {
+	bb := b.Bytes()
+	h := uint(0)
+	for _, l := range bb {
+		h = h ^ ((h << 5) + (h >> 2) + uint(l))
+	}
+	bLock.RLock()
+	defer bLock.RUnlock()
+
+	if bList == nil {
+		return nil, fmt.Errorf("No strings set in the bucket")
+	}
+
+	pos := h % uint(len(bList))
+	if s := bList[pos]; s != nil {
+		return s, nil
+	} else {
+		return nil, fmt.Errorf("There is no string set at bucket pos %d", pos)
+	}
+}
