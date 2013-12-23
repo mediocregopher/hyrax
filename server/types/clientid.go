ce0a3876eb75b024048227f38c9db40bca773a50
Brian Picciano <bgp2396@bellsouth.net>
2013-12-23 18:16:20 -0500

    make clientids' byte representation more readable

--- server/types/clientid.go
+++ server/types/clientid.go
@@ -1,8 +1,7 @@
 package types
 
 import (
-	"encoding/binary"
-	"fmt"
+	"strconv"
 )
 
 // ClientId is a unique value that's given to every client of this hyrax node
@@ -10,10 +9,7 @@ type ClientId uint64
 
 // Implements Bytes for the Byter interface
 func (cid ClientId) Bytes() []byte {
-	size := binary.Size(cid)
-	buf := make([]byte, size)
-	binary.PutUvarint(buf, uint64(cid))
-	return buf
+	return []byte(strconv.FormatUint(uint64(cid), 16))
 }
 
 // Implements Uint64 for the Uint64er interface
@@ -24,11 +20,12 @@ func (cid ClientId) Uint64() uint64 {
 // Given a byte slice (presumably returned from calling Bytes on a ClientId),
 // returns the corresponding ClientId, and possibly an error if shit is fucked
 func ClientIdFromBytes(b []byte) (ClientId, error) {
-	cid, i := binary.Uvarint(b)
-	if i <= 0 {
-		return 0, fmt.Errorf("Error reading ClientId from bytes: %v", b)
+	n, err := strconv.ParseUint(string(b), 16, 64)
+	if err != nil {
+		return 0, err
 	}
-	return ClientId(cid), nil
+
+	return ClientId(n), nil
 }
 
 // Given a uint64 (presumably returned from calling Uint64() on a ClientId),
